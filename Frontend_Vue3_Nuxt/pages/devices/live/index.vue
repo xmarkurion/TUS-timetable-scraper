405925805419ff622afcb503634cbbc2d0e80983
Markurion <xmarkurion@gmail.com>
2024-09-09 17:44:45 +0100

    tine

--- Frontend_Vue3_Nuxt/pages/devices/live/index.vue
+++ Frontend_Vue3_Nuxt/pages/devices/live/index.vue
@@ -1,3 +1,30 @@
+<script setup>
+import useTableTool from '@/composables/tableTool.ts'
+const dataSource = await queryContent('devices').findOne()
+const { data, dayOff,currentDay } = useTableTool(dataSource)
+
+const today = computed(()=>{
+    // get a way to get day dynamic
+    return data.Monday
+})
+
+ // Curent time
+ const currentTime = computed(()=>{
+        const today = new Date();
+        // const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        // const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        // const dateTime = date +' '+ time;
+        return today.getHours() + ":" + today.getMinutes();
+})
+
+</script>
+
 <template>
     Live 
+    {{ today }}
+    <br>
+    Time
+    {{ currentTime }}
+
+    
 </template>
