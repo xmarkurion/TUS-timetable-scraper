9cd3efe82e5fde7f4ef6002383b408b253a97e99
xmarkurion <xmarkurion@gmail.com>
2024-09-10 00:27:26 +0100

    a live version

--- Frontend_Vue3_Nuxt/pages/devices/live/index.vue
+++ Frontend_Vue3_Nuxt/pages/devices/live/index.vue
@@ -4,10 +4,14 @@ import { useNow } from '@vueuse/core'
 import Lecture from '@/components/markurion/Lecture'
 const data = await queryContent('devices').findOne()
 
+// Get the current date and time reactively
+// const now = useNow({ interval: 60000 }) // Update every minute
+const now = useNow() // Update every minute
+
 // Get the current day dynamically
 const currentDayName = computed(() => {
   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  return days[new Date().getDay()]
+  return days[now.value.getDay()]
 })
 
 // Get activities for the current day
@@ -15,17 +19,14 @@ const todayActivities = computed(() => {
   return data[currentDayName.value] || []
 })
 
-
 // Get the current time
 const currentTime = computed(() => {
-  const today = new Date()
-  return today.getHours() + ":" + today.getMinutes()
+  return now.value.getHours() + ":" + now.value.getMinutes()
 })
 
 // Compare current time with activity times
-  const upcomingActivity = computed(() => {
-  const now = new Date()
-  const nowTime = now.getHours() * 60 + now.getMinutes()
+const upcomingActivity = computed(() => {
+  const nowTime = now.value.getHours() * 60 + now.value.getMinutes()
 
   for (const activity of todayActivities.value) {
     const [startHour, startMinute] = activity.Start.split(':').map(Number)
@@ -37,34 +38,55 @@ const currentTime = computed(() => {
       const timeLeft = endTime - nowTime
       return { ...activity, status: 'ongoing', timeLeft }
     } else if (nowTime < startTime) {
-        const timeTo = startTime - nowTime
+      const timeTo = startTime - nowTime
       return { ...activity, status: 'upcoming', timeTo }
     }
   }
 
   return null
 })
-
 </script>
 
 <template>
-  <div>
-    Live Time: {{ currentTime }}
-    <div v-if="upcomingActivity">
-      <div v-if="upcomingActivity.status === 'ongoing'">
-        Current Activity: {{ upcomingActivity.Activity }}<br>
-            <Lecture :data="upcomingActivity"/>
-        Time Left: {{ upcomingActivity.timeLeft }} minutes
-      </div>
-      <div v-else>
-        Upcoming Activity: {{ upcomingActivity.Activity }}<br>
-         <Lecture :data="upcomingActivity"/>
-        Starts at: {{ upcomingActivity.Start }} <br>
-        Time left to start: {{  upcomingActivity.timeTo }} minutes
-      </div>
+  <div class="main">
+    <div class="time">
+        {{ currentTime }} 
     </div>
-    <div v-else>
-      No more activities for today.
+    <div class="info">
+        <div v-if="upcomingActivity">
+            <div v-if="upcomingActivity.status === 'ongoing'">
+              Current Activity: {{ upcomingActivity.Activity }}<br>
+              <Lecture :data="upcomingActivity"/>
+              Time Left: {{ upcomingActivity.timeLeft }} minutes
+            </div>
+            <div v-else>
+             <div>
+                Upcoming Activity: {{ upcomingActivity.Activity }}
+             </div>
+              <Lecture :data="upcomingActivity"/>
+              Starts at: {{ upcomingActivity.Start }} <br>
+              Time left to start: {{ upcomingActivity.timeTo }} minutes
+            </div>
+          </div>
+          <div v-else>
+            No more activities for today.
+          </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.time {
+    display: flex;
+    justify-content: center;
+    border: 2px darkcyan solid;
+}
+
+.info{
+    display: flex;
+    justify-content: center;
+    padding: 10px;
+}
+
+
+</style>
