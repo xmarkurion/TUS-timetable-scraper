abed9accea1d3d2f1d46fc1b0db6a819f224eff3
Markurion <xmarkurion@gmail.com>
2024-09-10 12:40:13 +0100

    layout

--- Frontend_Vue3_Nuxt/pages/devices/live/index.vue
+++ Frontend_Vue3_Nuxt/pages/devices/live/index.vue
@@ -6,7 +6,20 @@ const data = await queryContent('devices').findOne()
 
 // Get the current date and time reactively
 // const now = useNow({ interval: 60000 }) // Update every minute
+
 const now = useNow() // Update every minute
+// const now = ref(new Date(1726247448000))
+
+// Format the current time as hh:mm:ss
+const formattedTime = computed(() => {
+  const hours = String(now.value.getHours()).padStart(2, '0')
+  const minutes = String(now.value.getMinutes()).padStart(2, '0')
+  const seconds = String(now.value.getSeconds()).padStart(2, '0')
+  return {
+    hm: `${hours}:${minutes}`,
+    seconds,
+  }
+})
 
 // Get the current day dynamically
 const currentDayName = computed(() => {
@@ -14,9 +27,20 @@ const currentDayName = computed(() => {
   return days[now.value.getDay()]
 })
 
-// Get activities for the current day
+// // Get activities for the current day
+// const todayActivities = computed(() => {
+//   return data[currentDayName.value] || []
+// })
+
+const amountOfTodayActivities = computed(()=>{
+  return todayActivities.value.length
+})
+
 const todayActivities = computed(() => {
-  return data[currentDayName.value] || []
+  return (data[currentDayName.value] || []).map((activity, index) => ({
+    ...activity,
+    key: index
+  }))
 })
 
 // Get the current time
@@ -50,26 +74,56 @@ const upcomingActivity = computed(() => {
 <template>
   <div class="main">
     <div class="time">
-        {{ currentTime }} 
+        <span>{{ formattedTime.hm  }}</span>
+        <span class="text-xs">:{{formattedTime.seconds}}</span>
     </div>
     <div class="info">
         <div v-if="upcomingActivity">
+          
             <div v-if="upcomingActivity.status === 'ongoing'">
-              Current Activity: {{ upcomingActivity.Activity }}<br>
-              <Lecture :data="upcomingActivity"/>
-              Time Left: {{ upcomingActivity.timeLeft }} minutes
+              <div class="py-2 text-center">
+                Current Activity
+              </div>
+              <div class="borderr">
+                <Lecture :data="upcomingActivity"/>
+              </div>
+              
+              <div class="py-2 text-center">
+                Time Left: {{ upcomingActivity.timeLeft }} minutes
+              </div>
+
+              <div class="py-2 text-center">
+                <hr>
+              </div>
+              <div v-if="(upcomingActivity.key+1) !== amountOfTodayActivities" class="py-2 text-center">
+                Next:
+                <Lecture :data="todayActivities[upcomingActivity.key + 1]"/>
+              </div>
             </div>
+
             <div v-else>
-             <div>
-                Upcoming Activity: {{ upcomingActivity.Activity }}
+             <div class="py-2 text-center">
+                Upcoming Activity
              </div>
               <Lecture :data="upcomingActivity"/>
-              Starts at: {{ upcomingActivity.Start }} <br>
-              Time left to start: {{ upcomingActivity.timeTo }} minutes
+              <div class="py-2 text-center">
+                Starts at: {{ upcomingActivity.Start }} <br>
+              </div>
+              <div class="py-2 text-center">
+                <hr>
+              </div>
+              <div class="py-2 text-center">
+                Time left to start: {{ upcomingActivity.timeTo }} minutes
+              </div>
             </div>
           </div>
           <div v-else>
-            No more activities for today.
+            <div class="pt-2 text-center">
+              No more activities for today.
+            </div>
+            <div class="pt-2 text-center">
+              Go Home and enjoy the life before 9-5 lifestyle...
+            </div>
           </div>
     </div>
   </div>
@@ -79,13 +133,19 @@ const upcomingActivity = computed(() => {
 .time {
     display: flex;
     justify-content: center;
+    align-items: center;
     border: 2px darkcyan solid;
 }
 
 .info{
     display: flex;
+    flex-direction: row;
     justify-content: center;
     padding: 10px;
+}
+
+.borderr{
+  border: 2px seagreen dotted;
 }
 
 
