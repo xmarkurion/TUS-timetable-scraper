a6a9c63992432d2ce5e476f0da4587d101bcd2fc
xmarkurion <xmarkurion@gmail.com>
2024-09-11 23:42:10 +0100

    view packed into component

---
+++ Frontend_Vue3_Nuxt/components/markurion/WeekView.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+import Break from '../../components/markurion/Break.vue'
+import useTableTool from '@/composables/tableTool'
+import type { ParsedContent } from '@nuxt/content';
+
+const props = defineProps<{
+    data: any
+}>()
+
+// const dataSource = await queryContent('devices').findOne()
+const { data, dayOff,currentDay } = useTableTool(props.data)
+
+</script>
+
+<template>
+  <div class="header">
+    <NuxtLink :to="'/devices/live'">
+      <Card>
+        <CardHeader>
+          <CardTitle> Now Playing {{ currentDay }}</CardTitle>
+          <CardDescription>Click to see live what's now what's next.</CardDescription>
+        </CardHeader>
+      </Card>
+    </NuxtLink>
+  </div>
+
+  <div class="main"> 
+  <Tabs :default-value="currentDay" class="w-[400px]">
+    <TabsList>
+      <TabsTrigger value="Monday">
+        Monday
+      </TabsTrigger>
+      <TabsTrigger value="Tuesday">
+        Tuesday
+      </TabsTrigger>
+      
+      <TabsTrigger value="Wednesday">
+        Wednesday
+      </TabsTrigger>
+
+      <TabsTrigger value="Thursday">
+        Thursday
+      </TabsTrigger>
+
+      <TabsTrigger value="Friday">
+        Friday
+      </TabsTrigger>
+
+      <TabsTrigger v-if="dayOff" value="">
+      </TabsTrigger>
+
+    </TabsList>
+    <TabsContent value="Monday">
+      <Break :day="data.Monday"/>
+    </TabsContent>
+
+    <TabsContent value="Tuesday">
+        <Break :day="data.Tuesday"/>
+    </TabsContent>
+
+    <TabsContent value="Wednesday">
+        <Break :day="data.Wednesday"/>
+    </TabsContent>
+   
+    <TabsContent value="Thursday">
+        <Break :day="data.Thursday"/>
+    </TabsContent>
+
+    <TabsContent value="Friday">
+        <Break :day="data.Friday"/>
+    </TabsContent>
+    <TabsContent v-if="dayOff" value="" >
+      <div class="text-center pt-[50px]">Enjoy the weekend.</div>
+  </TabsContent>
+
+
+  </Tabs>
+
+  <!-- {{ data }} -->
+</div>
+</template>
+
+<style scoped>
+.header{
+  display: flex;
+  justify-content: center;
+}
+
+.main{
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+}
+
+</style>
--- Frontend_Vue3_Nuxt/pages/cloud/index.vue
+++ Frontend_Vue3_Nuxt/pages/cloud/index.vue
@@ -2,198 +2,141 @@
 import { computed } from 'vue'
 import { useNow } from '@vueuse/core'
 import Lecture from '@/components/markurion/Lecture'
-const data = await queryContent('cloud').findOne()
+import WeekView from '~/components/markurion/WeekView.vue';
 
-// Get the current date and time reactively
-// const now = useNow({ interval: 60000 }) // Update every minute
+const data = ref(null)
 
-const now = useNow() // Update every minute
-// const now = ref(new Date(1726017817000))
-
-// Format the current time as hh:mm:ss
-const formattedTime = computed(() => {
-  const hours = String(now.value.getHours()).padStart(2, '0')
-  const minutes = String(now.value.getMinutes()).padStart(2, '0')
-  const seconds = String(now.value.getSeconds()).padStart(2, '0')
-  return {
-    hm: `${hours}:${minutes}`,
-    seconds,
-  }
+onMounted(async ()=>{
+  data.value = await queryContent('cloud').findOne()
 })
 
-// Get the current day dynamically
-const currentDayName = computed(() => {
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  return days[now.value.getDay()]
-})
-
-const amountOfTodayActivities = computed(()=>{
-  return todayActivities.value.length
-})
-
-const todayActivities = computed(() => {
-  return (data[currentDayName.value] || []).map((activity, index) => ({
-    ...activity,
-    key: index
-  }))
-})
-
-// Get the current time
-const currentTime = computed(() => {
-  return now.value.getHours() + ":" + now.value.getMinutes()
-})
-
-// Compare current time with activity times
-const upcomingActivity = computed(() => {
-  const nowTime = now.value.getHours() * 60 + now.value.getMinutes()
-
-  for (const activity of todayActivities.value) {
-    const [startHour, startMinute] = activity.Start.split(':').map(Number)
-    const [endHour, endMinute] = activity.End.split(':').map(Number)
-    const startTime = startHour * 60 + startMinute
-    const endTime = endHour * 60 + endMinute
-
-    if (nowTime >= startTime && nowTime <= endTime) {
-      const timeLeft = endTime - nowTime
-      return { ...activity, status: 'ongoing', timeLeft }
-    } else if (nowTime < startTime) {
-      const timeTo = startTime - nowTime
-      return { ...activity, status: 'upcoming', timeTo }
-    }
-  }
-
-  return null
-})
 </script>
 
 <template>
-  <div class="main">
-    <div class="time">
-        <span>{{ formattedTime.hm  }}</span>
-        <span class="text-xs">:{{formattedTime.seconds}}</span>
-    </div>
-    <div class="info">
-        <div v-if="upcomingActivity">
-          
-            <div v-if="upcomingActivity.status === 'ongoing'">
-              <div class="py-2 text-center">
-                Current Activity
-              </div>
-              <div class="borderr">
-                <Lecture :data="upcomingActivity"/>
-              </div>
-              
-              <div class="py-2 text-center">
-                Time Left: {{ upcomingActivity.timeLeft }} minutes
-              </div>
-
-              <div class="py-2 text-center">
-                <hr>
-              </div>
-              <div v-if="(upcomingActivity.key+1) !== amountOfTodayActivities" class="py-2 text-center">
-                <div>
-                  Up Next
-                </div>
-                <div class="pt-2">
-                  <Lecture :data="todayActivities[upcomingActivity.key + 1]"/>
-                </div>
-              </div>
-            </div>
-
-            <div v-else>
-             <div class="py-2 text-center">
-                Upcoming Activity
-             </div>
-              <Lecture :data="upcomingActivity"/>
-              <div class="py-2 text-center">
-                Starts at: {{ upcomingActivity.Start }} <br>
-              </div>
-              <div class="py-2 text-center">
-                <hr>
-              </div>
-              <div class="py-2 text-center">
-                Time left to start: {{ upcomingActivity.timeTo }} minutes
-              </div>
-            </div>
-          </div>
-          <div v-else>
-            <div class="pt-2 text-center">
-              No more activities for today.
-            </div>
-            <div class="pt-2 text-center">
-              Go Home and read something, knowledge is all you need.
-            </div>
-            <div class="pt-2 text-center">
-              “Wherever you go, there you are.”
-              <span class="quote">Thomas à Kempi</span>
-            </div>
-            <video autoplay playsinline muted loop width="400px" class="video">  
-              <source src="/images/vid.webm" type="video/webm" >  
-              Sorry, your browser doesn't support HTML5 video.  
-            </video> 
-          </div>
-    </div>
+  <div v-if="!data"class="loading">Loading&#8230;</div>
+  <div v-else>
+    <WeekView :data="data" v-if="data"/>
   </div>
 </template>
 
 <style scoped>
-html, body {
-  margin: 0;
-  padding: 0;
-  background-color: black; /* Set background color to black */
-  color: white; /* Optional: Set text color to white for better contrast */
-  height: 100%;
+/* Absolute Center Spinner */
+.loading {
+  position: fixed;
+  z-index: 999;
+  height: 2em;
+  width: 2em;
+  overflow: visible;
+  margin: auto;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
 }
 
-.time {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 2px darkcyan solid;
-    width: 120px;
-}
-
-.info{
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    padding: 10px;
-}
-
-.borderr{
-  border: 5px white dotted;
-  padding: 10px;
-}
-
-.video {
-  display: flex;
-  align-items: center;
-  justify-content: baseline;
-  animation: zoomInOut 5s infinite; /* Apply the zoom-in and zoom-out animation */
-}
-
-.main {
-  background-color: black; /* Set background color to black */
-  color: white; /* Optional: Set text color to white for better contrast */
-  min-height: 100vh; /* Ensure the main container takes up the full height of the viewport */
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+/* Transparent Overlay */
+.loading:before {
+  content: '';
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
+  height: 100%;
+  background-color: rgba(0,0,0,0.3);
 }
 
-@keyframes zoomInOut {
-  0%, 100% {
-    transform: scale(1); /* Initial and final state: no scaling */
+/* :not(:required) hides these rules from IE9 and below */
+.loading:not(:required) {
+  /* hide "loading..." text */
+  font: 0/0 a;
+  color: transparent;
+  text-shadow: none;
+  background-color: transparent;
+  border: 0;
+}
+
+.loading:not(:required):after {
+  content: '';
+  display: block;
+  font-size: 10px;
+  width: 1em;
+  height: 1em;
+  margin-top: -0.5em;
+  -webkit-animation: spinner 1500ms infinite linear;
+  -moz-animation: spinner 1500ms infinite linear;
+  -ms-animation: spinner 1500ms infinite linear;
+  -o-animation: spinner 1500ms infinite linear;
+  animation: spinner 1500ms infinite linear;
+  border-radius: 0.5em;
+  -webkit-box-shadow: rgba(0, 0, 0, 0.75) 1.5em 0 0 0, rgba(0, 0, 0, 0.75) 1.1em 1.1em 0 0, rgba(0, 0, 0, 0.75) 0 1.5em 0 0, rgba(0, 0, 0, 0.75) -1.1em 1.1em 0 0, rgba(0, 0, 0, 0.5) -1.5em 0 0 0, rgba(0, 0, 0, 0.5) -1.1em -1.1em 0 0, rgba(0, 0, 0, 0.75) 0 -1.5em 0 0, rgba(0, 0, 0, 0.75) 1.1em -1.1em 0 0;
+  box-shadow: rgba(0, 0, 0, 0.75) 1.5em 0 0 0, rgba(0, 0, 0, 0.75) 1.1em 1.1em 0 0, rgba(0, 0, 0, 0.75) 0 1.5em 0 0, rgba(0, 0, 0, 0.75) -1.1em 1.1em 0 0, rgba(0, 0, 0, 0.75) -1.5em 0 0 0, rgba(0, 0, 0, 0.75) -1.1em -1.1em 0 0, rgba(0, 0, 0, 0.75) 0 -1.5em 0 0, rgba(0, 0, 0, 0.75) 1.1em -1.1em 0 0;
+}
+
+/* Animation */
+
+@-webkit-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
   }
-  50% {
-    transform: scale(0.8); /* Midpoint: zoom in */
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
   }
 }
-
-.quote{
-  font-size: 5px;
+@-moz-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
 }
-
+@-o-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
 </style>
